860fe1be544473fce8bb7b0cccd82bf277007ef3
John Hilbun <john.hilbun@comcast.net>
2020-04-01 08:58:40 -0700

    improved performance
     - replace for/for with find max1, max2

--- PA1/02/max_pairwise_product.cpp
+++ PA1/02/max_pairwise_product.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> // JDH added
 
 using std::vector;
 using std::cin;
 using std::cout;
+using std::find;	// JDH added
 
 // JDH change vector values to long instead of int
 long MaxPairwiseProduct(const vector<long>& numbers) {
   long result = 0;	// WAS int
+/*
   int n = numbers.size();
   for (int i = 0; i < n; ++i) {
     for (int j = i + 1; j < n; ++j) {
@@ -16,6 +19,15 @@ long MaxPairwiseProduct(const vector<long>& numbers) {
       }
     }
   }
+  */
+  long max1 = 0;
+  long max2 = 0;
+  vector<long> local_numbers;
+  local_numbers = numbers;
+  max1 = *max_element (local_numbers.begin(), local_numbers.end());
+  local_numbers.erase(find(local_numbers.begin(), local_numbers.end(), max1));
+  max2 = *max_element (local_numbers.begin(), local_numbers.end());
+  result = max1 * max2;
   return result;
 }
 
