752844a8dc778cc1490c8fe962dbdd2eed5c862b
John Hilbun <john.hilbun@comcast.net>
2020-04-10 14:02:15 -0700

    Non-recursive implementation : passes UCSD testing

--- PA2/01/fibonacci.py
+++ PA2/01/fibonacci.py
@@ -1,9 +1,17 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
+    # Naive - original
+    #if (n <= 1):
+    #    return n
+    #return calc_fib(n - 1) + calc_fib(n - 2)
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    # Smarter
+    F = []
+    F.append(0)
+    F.append(1)
+    for i in range (2, n+1):
+        F.append(F[i-1] + F[i-2])
+    return F[n]
 
 n = int(input())
 print(calc_fib(n))
