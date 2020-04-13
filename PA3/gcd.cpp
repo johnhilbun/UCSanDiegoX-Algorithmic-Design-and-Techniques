158a91ad2169b58ff22aa72ad0c6ac4c50cf8023
John Hilbun <john.hilbun@comcast.net>
2020-04-13 16:52:32 -0700

    Added fast implementation

--- PA3/gcd.cpp
+++ PA3/gcd.cpp
@@ -12,9 +12,21 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+long gcd_fast (long dividend, long divisor) {	// JDH added this function
+	long result, remainder;
+
+	if (divisor == 0)
+		return dividend;
+
+	remainder = dividend % divisor;
+	result = gcd_fast (divisor, remainder);
+	return (result);
+}
+
 int main() {
-  int a, b;
+  long a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  // std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_fast(a, b) << std::endl;
   return 0;
 }
