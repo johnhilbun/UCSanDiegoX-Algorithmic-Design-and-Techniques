4f5e24b077df5489e66a6a12c38ed4cf15753037
John Hilbun <john.hilbun@comcast.net>
2020-04-13 17:09:34 -0700

    Added fast implementation

--- PA3/gcd.py
+++ PA3/gcd.py
@@ -7,10 +7,17 @@ def gcd_naive(a, b):
         if a % d == 0 and b % d == 0:
             if d > current_gcd:
                 current_gcd = d
-
     return current_gcd
 
+def gcd_fast (a, b):
+    if (b == 0):
+        return a
+
+    remainder = a % b
+    return (gcd_fast (b, remainder))
+
 if __name__ == "__main__":
-    input = sys.stdin.read()
+    input = sys.stdin.readline()
     a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    # print(gcd_naive(a, b))
+    print (gcd_fast (a, b))
