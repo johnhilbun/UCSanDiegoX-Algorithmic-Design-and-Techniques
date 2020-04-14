51455d3db5a80886eb6a9078b8966175d84c7b8d
John Hilbun <john.hilbun@comcast.net>
2020-04-14 08:56:08 -0700

    Fast implementation - passes all tests

--- PA4/lcm.cpp
+++ PA4/lcm.cpp
@@ -8,9 +8,25 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+long lcd_fast (long dividend, long divisor) {	// JDH added
+	long remainder = 0;
+
+	if (divisor == 0)	// was <= 1
+		return dividend;
+
+	remainder = dividend % divisor;
+	return (lcd_fast (divisor, remainder));
+}
+
+long long lcm_fast (long a, long b) {	// JDH added
+
+	return (a * b / lcd_fast (a, b));
+}
+
 int main() {
-  int a, b;
+  long a, b;	// JDH original int
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  // std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
