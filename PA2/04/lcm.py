7a25ab758d200d2ecda711f16fe32401765350ab
John Hilbun <john.hilbun@comcast.net>
2020-04-14 09:27:40 -0700

    Fast implementation

--- PA2/04/lcm.py
+++ PA2/04/lcm.py
@@ -5,11 +5,21 @@ def lcm_naive(a, b):
     for l in range(1, a*b + 1):
         if l % a == 0 and l % b == 0:
             return l
-
     return a*b
 
+def lcd_fast (dividend, divisor):   # JDH added
+    if (divisor == 0):
+        return dividend
+    remainder = dividend % divisor
+    return lcd_fast(divisor, remainder)
+
+def lcm_fast (a, b):    # JDH added
+    result = (a * b // lcd_fast (a, b)) # JDH use // so it's not a float
+    return result
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    input = sys.stdin.readline()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    # print(lcm_naive(a, b)) # JDH original
+    print(lcm_fast(a, b))   # JDH added
 
